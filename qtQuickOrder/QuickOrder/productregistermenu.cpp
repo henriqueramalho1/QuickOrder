658b6950106065bd1794b677661d4cae6b219b2c
Henrique <henrique.ramalho2002@gmail.com>
2021-12-02 14:30:59 -0300

    produto registrado no banco de dados

--- qtQuickOrder/QuickOrder/productregistermenu.cpp
+++ qtQuickOrder/QuickOrder/productregistermenu.cpp
@@ -1,6 +1,7 @@
 #include "productregistermenu.h"
 #include "./ui_productregistermenu.h"
 #include "product.h"
+#include <QMessageBox>
 
 #include <iostream>
 
@@ -15,17 +16,14 @@ ProductRegisterMenu::~ProductRegisterMenu(){
 
 void ProductRegisterMenu::on_registerButton_clicked()
 {
-    printf("clicked\n");
-    Product *newProduct = new Product(ui->nameInput->text().toStdString());
-    newProduct->addIngredient(ui->ingredientInput->text().toStdString());
-    newProduct->addIngredient(ui->ingredientInput_2->text().toStdString());
-    newProduct->addIngredient(ui->ingredientInput_3->text().toStdString());
-    newProduct->addIngredient(ui->ingredientInput_4->text().toStdString());
-    newProduct->addIngredient(ui->ingredientInput_5->text().toStdString());
+    QString productName = ui->nameInput->text();
+    QString ingredient1 = ui->ingredientInput->text();
+    QString ingredient2 = ui->ingredientInput_2->text();
+    QString ingredient3 = ui->ingredientInput_3->text();
+    QString ingredient4 = ui->ingredientInput_4->text();
+    QString ingredient5 = ui->ingredientInput_5->text();
 
-    std::cout << newProduct->getName() << std::endl;
-    //guardar no banco
-    delete newProduct;
+    addToDatabase(productName, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5);
 
     close();
 }
@@ -33,6 +31,30 @@ void ProductRegisterMenu::on_registerButton_clicked()
 void ProductRegisterMenu::on_backButton_clicked()
 {
     close();
+}
+
+void ProductRegisterMenu::addToDatabase(QString productName, QString ingredient1, QString ingredient2, QString ingredient3, QString ingredient4, QString ingredient5)
+{
+    if(productName == "" && ingredient1 == "" && ingredient2 == "" && ingredient3 == "" && ingredient4 == "" && ingredient5 == "")
+    {
+        QMessageBox::warning(this,"", "Não é possível cadastrar esse produto, verifique os campos");
+        return;
+    }
+
+    QSqlQuery query;
+
+    query.prepare("insert into product_tb (name, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5) values (?, ?, ?, ?, ?, ?)");
+    query.addBindValue(productName);
+    query.addBindValue(ingredient1);
+    query.addBindValue(ingredient2);
+    query.addBindValue(ingredient3);
+    query.addBindValue(ingredient4);
+    query.addBindValue(ingredient5);
+
+    if(query.exec())
+    {
+        QMessageBox::information(this, "", "Produto cadastrado com sucesso.");
+    }
 }
 
 
